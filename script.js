b93841425d962ba71b842610138493c71c2538a7
ByrneHub <116261123+ByrneHub@users.noreply.github.com>
2024-11-23 21:06:01 +0000

    Added Buttons

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-containter");
+const listContainer = document.getElementById("list-container");
 function addTask(){
     if(inputBox.value === ''){
         alert("You need to add something!");
